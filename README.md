cc72afbf7692bdb03b7ebf262a43c2e47fcb0a89
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-08-31 14:14:44 +0200

    gjorde svar på mitt namn

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # prog1-ovn
  Övningar för programmering 1
+
+Detta är ett övningsrespository för kursen programmering 1.
---
+++ Upgift 1,4.py
@@ -0,0 +1,6 @@
+# upgift1.4.py
+
+name = input('vad heter du?')
+print('hej', name)
+efternamn = input('vad är ditt efternamn?')
+print('hej',name,efternamn)
