602c3311fc8d92a7c6736866be5e148ca9c095ad
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-09-21 14:24:02 +0200

    Update 2,8.py

--- 2,8.py
+++ 2,8.py
@@ -1,5 +1,5 @@
-import random 
+import random #gör att man kan använda variabler som är random
 print('tärningarna är kastade')
-n = random.randint(1,6)
+n = random.randint(1,6) #gör att mam kan få mellan talen 1 fram till 6
 m = random.randint(1,6)
 print('du fick', n, m,'eller', n+m)
