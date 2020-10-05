fb456de53e48363b33137e09128f240810f0e081
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-10-05 14:46:12 +0200

    Update 3,2.py

--- 3,2.py
+++ 3,2.py
@@ -3,6 +3,6 @@ biljett=float(input('hur mycket ett gyms biljett kostar:'))
 antal=int(input('hur många gånger du tänker gå på gym på ett år:'))
 x=biljett*antal
 if ars<x:
-    print('då lönar det sig med att ta årskort') 
+    print('det lönar det sig med att ta årskort') 
 else:
-    print('då lönar det sig inte med årskort')
+    print('det lönar det sig inte med årskort')
